533ee214c19d843b1f673da50a6a96cfa848bec5
Egstad <jordanegstad@gmail.com>
2020-11-10 23:03:51 -0800

    :v: 0.0.1

--- index.js
+++ index.js
@@ -1,216 +1,184 @@
-const options = {
-  horizontal: 0
-}
-
 export default class detectScroll {
-  constructor(el, options) {
-    this.x = null;
-    this.y = null;
+  constructor(el, {
+    horizontal = false,
+    vertical = true,
+  } = {}) {
     this.el = (el || window);
-    this.isWindow = (this.el === window);
-    // this.isHorizontal = (options.horizontal || false)
+    this.x = this.getX();
+    this.y = this.getY();
+    this.isWindow = (window === el);
+    this.isScrolling = false;
+    this.isVertical = vertical;
+    this.isHorizontal = horizontal;
+    this.rafId = null;
+    this.rafTick = 0;
+    this.rafKilled = false;
 
-    this.watch()
+    this.setup();
+    this.watchX();
+    this.watchY();
   }
+
+  setup() {
+    this.el.addEventListener('scroll', this.onScroll.bind(this), false);
+  }
+
+  teardown() {
+    this.el.removeEventListener('scroll', this.onScroll.bind(this), false);
+    window.clearTimeout(this.timeout);
+    window.cancelAnimationFrame(this.rafId);
+    this.rafKilled = true;
+  }
+
+  scrollTimeout() {
+    this.timeout = setTimeout(() => {
+      this.onScrollEnd();
+    }, 120);
+  }
+
   getY() {
-    return (this.isWindow ? window.scrollY : this.el.scrollTop);
+    return (this.isWindow ? window.pageYOffset : this.el.scrollTop);
   }
-  getX() {
-    return (this.isWindow ? window.scrollX : this.el.scrollLeft);
-  }
-  startY() {
-    const currentY = this.getY();
-    const isStill = (currentY === this.y);
-    const isScroll = (currentY !== this.y);
-    const isScrollUp = (currentY < this.y);
-    const isScrollDown = (currentY > this.y);
 
-    // Fire blanket scroll event
-    if (isStill) {
-      console.log('idle', currentY)
-    } else if (isScroll) {
-      // console.log('idle', currentY)
+  getYMax() {
+    let max = null;
+
+    if (this.isWindow) {
+      max = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.documentElement.clientHeight,
+      );
+    } else {
+      max = Math.max(
+        this.el.scrollHeight,
+        this.el.offsetHeight,
+        this.el.clientHeight,
+      );
     }
 
-    // vertical
-    // if (isScrollDown) {
-    //   console.log('down', currentY)
-
-    // } else if (isScrollUp) {
-    //   console.log('up', currentY)
-    // }
-
-    this.y = currentY;
+    return (max - window.innerHeight);
   }
-  watch(timestamp) {
-    this.startY()
-    window.requestAnimationFrame(this.watch.bind(this));
-  }
-  pause() {
 
+  isMaxY() {
+    return (this.getY() >= this.getYMax());
+  }
+
+  isMinY() {
+    return (this.getY() <= 0);
+  }
+
+  getX() {
+    return (this.isWindow ? window.pageXOffset : this.el.scrollLeft);
+  }
+
+  getXMax() {
+    let max = null;
+
+    if (this.isWindow) {
+      max = Math.max(
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.offsetWidth, document.documentElement.offsetWidth,
+        document.documentElement.clientWidth,
+      );
+    } else {
+      max = Math.max(
+        this.el.scrollWidth,
+        this.el.offsetWidth,
+        this.el.clientWidth,
+      );
+    }
+
+    return (max - window.innerWidth);
+  }
+
+  isMaxX() {
+    return (this.getX() >= this.getXMax());
+  }
+
+  isMinX() {
+    return (this.getX() <= 0);
+  }
+
+  watchX() {
+    const x = this.getX();
+    const scrolling = (x !== this.x);
+    const scrollingLeft = (x < this.x);
+    const scrollingRight = (x > this.x);
+    const atEnd = (x !== this.x && this.isMaxX());
+    const atStart = (x !== this.x && this.isMinX());
+
+    if (scrolling) {
+      this.onScrollStart();
+    }
+
+    if (scrollingLeft) {
+      this.dispatch('scrollLeft');
+    } else if (scrollingRight) {
+      this.dispatch('scrollRight');
+    }
+
+    if (atEnd) {
+      this.dispatch('scrollEnd');
+    } else if (atStart) {
+      this.dispatch('scrollBeginning');
+    }
+
+    this.x = x;
+  }
+
+  watchY() {
+    const y = this.getY();
+    const scrolling = (y !== this.y);
+    const scrollingUp = (y < this.y);
+    const scrollingDown = (y > this.y);
+    const atEnd = (y !== this.y && this.isMaxY());
+    const atStart = (y !== this.y && this.isMinY());
+
+    if (scrolling) {
+      this.onScrollStart();
+    }
+    if (scrollingDown) {
+      this.dispatch('scrollDown');
+    } else if (scrollingUp) {
+      this.dispatch('scrollUp');
+    }
+
+    if (atEnd) {
+      this.dispatch('scrollBottom');
+    } else if (atStart) {
+      this.dispatch('scrollTop');
+    }
+
+    this.y = y;
+  }
+
+  dispatch(type) {
+    this.el.dispatchEvent(new CustomEvent(type));
+  }
+
+  onScroll() {
+    if (this.rafKilled) return;
+
+    this.rafId = window.requestAnimationFrame(() => {
+      if (this.isHorizontal) this.watchX();
+      if (this.isVertical) this.watchY();
+
+      // refresh timeout, watch for scroll stop
+      window.clearTimeout(this.timeout);
+      this.scrollTimeout();
+    });
+  }
+
+  onScrollStart() {
+    if (!this.isScrolling) {
+      this.dispatch('scrollStart');
+      this.isScrolling = true;
+    }
+  }
+
+  onScrollEnd() {
+    this.dispatch('scrollStop');
+    this.isScrolling = false;
   }
 }
-
-
-
-
-// const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-// const watch = () => {
-//   mediaQuery.addEventListener("change", get, false);
-//   get()
-// }
-
-// const teardown = () => {
-//   mediaQuery.removeEventListener("change", get, false);
-// }
-
-// export const get = () => {
-//   const theme = ["dark", "light", "no-preference"].find(
-//     (scheme) => window.matchMedia(`(prefers-color-scheme: ${scheme})`).matches
-//   );
-
-//   const event = new CustomEvent("colorSchemeUpdated", {
-//     detail: {
-//       theme,
-//     },
-//   });
-//   window.dispatchEvent(event);
-// }
-
-// export default {
-//   watch,
-//   teardown,
-//   get
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const html = document.documentElement
-
-// const scroll = {
-//   docX: 0,
-//   docY: 0,
-//   current: null,
-//   timeout: null,
-//   scrollHasStarted: null,
-
-//   init() {
-//     this.bindEvents()
-//     this.setScrollClasses()
-//   },
-//   bindEvents() {
-//     window.addEventListener(
-//       'scroll',
-//       () => {
-//         window.requestAnimationFrame(() => {
-//           this.setScrollClasses()
-//         })
-//         window.clearTimeout(this.timeout)
-//         this.beginTimeout()
-//       },
-//       false
-//     )
-//   },
-//   getScrollY() {
-//     return window.scrollY
-//   },
-//   beginTimeout() {
-//     this.timeout = setTimeout(() => {
-//       this.onScrollStop()
-//     }, 150)
-//   },
-//   getScrollBottom() {
-//     const max = Math.max(
-//       document.body.scrollHeight,
-//       document.documentElement.scrollHeight,
-//       document.body.offsetHeight,
-//       document.documentElement.offsetHeight,
-//       document.documentElement.clientHeight
-//     )
-
-//     return max - window.innerHeight
-//   },
-//   isScrollBottom() {
-//     return this.getScrollY() >= this.getScrollBottom()
-//   },
-//   isScrollTop() {
-//     return this.getScrollY() <= 0
-//   },
-//   setScrollClasses() {
-//     const current = this.getScrollY()
-//     const scrolling = current !== this.current
-//     const up = current < this.current
-//     const down = current > this.current
-//     const top = current !== this.current && this.isScrollBottom()
-//     const bottom = current !== this.current && this.isScrollTop()
-
-//     if (scrolling && !this.scrollHasStarted) {
-//       this.onScrollStart()
-//     }
-
-//     if (down) {
-//       this.onScrollDown()
-//     } else if (up) {
-//       this.onScrollUp()
-//     }
-
-//     if (top) {
-//       this.onScrollMax()
-//     } else if (bottom) {
-//       this.onScrollMin()
-//     }
-
-//     this.current = current
-//   },
-//   onScrollStart() {
-//     this.scrollHasStarted = true
-//     html.classList.add('is-scrolling')
-//     html.classList.remove(
-//       'is-scroll-top',
-//       'is-scroll-bottom',
-//       'is-scroll-near-top'
-//     )
-//   },
-//   onScrollStop() {
-//     this.scrollHasStarted = false
-//     html.classList.remove('is-scrolling')
-//     window.$nuxt.$store.dispatch('device/updateScrollCoords')
-//     window.$nuxt.$store.dispatch('device/updateDocDimensions')
-//   },
-//   onScrollDown() {
-//     html.classList.add('scrolling-down')
-//     html.classList.remove('scrolling-up')
-//   },
-//   onScrollUp() {
-//     html.classList.add('scrolling-up')
-//     html.classList.remove('scrolling-down')
-//   },
-//   onScrollMax() {
-//     html.classList.add('is-scroll-bottom')
-//     // html.classList.remove("is-scroll-top")
-//   },
-//   onScrollMin() {
-//     html.classList.add('is-scroll-top')
-//     html.classList.remove('is-scroll-near-top')
-//   }
-// }
-
-// window.onNuxtReady(() => {
-//   scroll.init()
-// })
-
-// export default scroll
--- test/app.js
+++ test/app.js
@@ -1,9 +1,89 @@
-import detectScroll from '../index.js'
+import DetectScroll from '../index';
 
-const horizontalScroll = document.querySelector('.h')
+/**
+ * VERTICAL SCROLL
+ */
+const scrollV = window;
+// eslint-disable-next-line no-unused-vars
+const scrollVInstance = new DetectScroll(scrollV);
+const tipV = document.querySelector('.tip.v');
+const vDir = tipV.querySelector('.direction');
+const vPos = tipV.querySelector('.state');
 
-// const window = new detectScroll(horizontalScroll, {
-//   horizontal: true
-// })
+function updateDirection(el, direction) {
+  if (el.innerText !== direction) {
+    // eslint-disable-next-line no-param-reassign
+    el.innerText = direction;
+  }
+}
 
-const window = new detectScroll()
+function updateState(el, position) {
+  if (el.innerText !== position) {
+    // eslint-disable-next-line no-param-reassign
+    el.innerText = position;
+  }
+}
+
+scrollV.addEventListener('scrollDown', () => {
+  updateDirection(vDir, 'down');
+  updateState(vPos, 'scrolling');
+});
+
+scrollV.addEventListener('scrollUp', () => {
+  updateDirection(vDir, 'up');
+  updateState(vPos, 'scrolling');
+});
+
+scrollV.addEventListener('scrollStop', () => {
+  updateState(vPos, 'paused');
+});
+
+scrollV.addEventListener('scrollTop', () => {
+  updateDirection(vDir, 'top');
+});
+
+scrollV.addEventListener('scrollBottom', () => {
+  updateDirection(vDir, 'bottom');
+});
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+const scrollH = document.querySelector('.h');
+// eslint-disable-next-line no-unused-vars
+const scrollHInstance = new DetectScroll(scrollH, { horizontal: true, vertical: false });
+const tipH = document.querySelector('.hori');
+const hDir = tipH.querySelector('.direction');
+const hPos = tipH.querySelector('.state');
+
+scrollH.addEventListener('scrollRight', () => {
+  updateDirection(hDir, 'right');
+  updateState(hPos, 'scrolling');
+});
+
+scrollH.addEventListener('scrollLeft', () => {
+  updateDirection(hDir, 'left');
+  updateState(hPos, 'scrolling');
+});
+
+scrollH.addEventListener('scrollStop', () => {
+  updateState(hPos, 'paused');
+});
+
+scrollH.addEventListener('scrollBeginning', () => {
+  updateDirection(hDir, 'beginning');
+});
+
+scrollH.addEventListener('scrollEnd', () => {
+  updateDirection(hDir, 'end');
+});
